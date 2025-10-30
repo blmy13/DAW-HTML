33a40616c629b491d84ec43e2ba274aef65eeddf
Alex <abarthel.daw1r25@lamerce.com>
2025-10-30 23:34:09 +0100

    .

--- HTML/Random/js/scripts.js
+++ HTML/Random/js/scripts.js
@@ -1,4 +1,47 @@
+let runningTotal = '';
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
 
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.textContent;
+
+        // 1. NÚMEROS, OPERADORS I PUNT        
+        if (button.classList.contains('btn_number') || 
+            button.classList.contains('btn_ope') || 
+            button.classList.contains('btn_dot')) { // <--- CORRECCIÓ / MILLORA
+                 
+            if (runningTotal === '' && (value === '+' || value === '*' || value === '/')) {
+                return; 
+            }
+            runningTotal += value;
+            display.value = runningTotal;
         
-
-    
+        //BOTÓ IGUAL
+        } else if (button.classList.contains('btn_equal')) {
+            try {
+                let result = eval(runningTotal);
+                runningTotal = result.toString(); 
+                display.value = runningTotal;
+            } catch {
+                display.value = 'Error';
+                runningTotal = '';
+            }
+        
+        //BOTÓ NETEJAR
+        } else if (button.classList.contains('btn_clear')) {
+            runningTotal = '';
+            display.value = '0';
+        
+        //BOTÓ ESBORRAR 
+        } else if (button.classList.contains('btn_del')) {
+            runningTotal = runningTotal.slice(0, -1);
+                        
+            if (runningTotal === '') {
+                display.value = '0';
+            } else {
+                display.value = runningTotal;
+            }
+        }
+    });
+});
